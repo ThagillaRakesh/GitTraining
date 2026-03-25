970c0bd0e6ac67692f629bdc9b4f1db0e6363793
ThagillaRakesh <thagillarakesh@gmail.com>
2026-03-25 07:36:53 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,10 +1,10 @@
+import sys
+
 def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    return False
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
 if __name__ == "__main__":
-    year = int(input("Enter year: "))
+    year = int(sys.argv[1])   # take argument
     if is_leap_year(year):
         print("Leap Year")
     else:
